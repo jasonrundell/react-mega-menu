28040fb25820e3c8f43591121d28513b715909d4
Jason Rundell <jason.rundell@gmail.com>
2020-09-02 13:53:04 -0400

    Hamburger moduled

--- src/components/Buttons/Hamburger/index.jsx
+++ src/components/Buttons/Hamburger/index.jsx
@@ -2,24 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 // CSS
-import './index.scss'
+import styles from './index.module.scss'
 
 const Hamburger = ({ label, state, onClick }) => {
   if (state === '') state = 'closed'
   return (
-    <button
-      className={`button--hamburger button--hamburger-${state}`}
-      onClick={onClick}
-    >
-      <div className="button--hamburger-slice-container">
-        <span className="button--hamburger-slice"></span>
-        <span className="button--hamburger-slice"></span>
-        <span className="button--hamburger-slice"></span>
-        <span className="button--hamburger-slice"></span>
+    <button className={`${styles['root']} ${styles[state]}`} onClick={onClick}>
+      <div className={styles['slice-container']}>
+        <span className={styles['slice']}></span>
+        <span className={styles['slice']}></span>
+        <span className={styles['slice']}></span>
+        <span className={styles['slice']}></span>
       </div>
       {label && (
-        <div className="button--hamburger-label-container">
-          <span className="button--hamburger-label">{label}</span>
+        <div className={styles['label-container']}>
+          <span className={styles['label']}>{label}</span>
         </div>
       )}
     </button>
