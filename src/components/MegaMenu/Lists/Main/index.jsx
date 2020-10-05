39bcf5bb9d9c9c9d223b7e65a71a665716fb21af
Jason Rundell <jason.rundell@gmail.com>
2020-10-05 16:54:13 -0400

    Rearranged props

--- src/components/MegaMenu/Lists/Main/index.jsx
+++ src/components/MegaMenu/Lists/Main/index.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types'
 // CSS
 import styles from './index.module.scss'
 
-const MainList = ({ children, ariaLabel, id }) => {
+const MainList = ({ id, children, ariaLabel }) => {
   return (
     <ul
+      id={id}
       role="menubar"
       aria-label={ariaLabel}
       className={styles['root']}
-      id={id}
     >
       {children}
     </ul>
@@ -22,9 +22,9 @@ MainList.defaultProps = {
 }
 
 MainList.propTypes = {
+  id: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
   ariaLabel: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
 }
 
 export default MainList
