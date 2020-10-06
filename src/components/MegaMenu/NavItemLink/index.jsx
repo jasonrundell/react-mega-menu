e00732b3af97208dc4b6c0c271fb176579fd2389
Jason Rundell <jason.rundell@gmail.com>
2020-10-06 00:21:01 -0400

    NavItemLink updates

--- src/components/MegaMenu/NavItemLink/index.jsx
+++ src/components/MegaMenu/NavItemLink/index.jsx
@@ -7,10 +7,34 @@ import { classNames } from '../../../utils/css'
 // CSS
 import styles from './index.module.scss'
 
-const NavItemLink = ({ id, role, href, className, children }) => {
-  const rootClasses = classNames(styles.root, className && className)
+const NavItemLink = ({
+  id,
+  role,
+  href,
+  isBack,
+  className,
+  onClick,
+  onKeyDown,
+  ariaHaspopup,
+  ariaControls,
+  children,
+}) => {
+  const rootClasses = classNames(
+    styles.root,
+    isBack && styles['back'],
+    className && className
+  )
   return (
-    <a id={id} role={role} href={href} className={rootClasses}>
+    <a
+      id={id}
+      role={role}
+      href={href}
+      className={rootClasses}
+      onClick={onClick}
+      onKeyDown={onKeyDown}
+      aria-haspopup={ariaHaspopup}
+      aria-controls={ariaControls}
+    >
       {children}
     </a>
   )
@@ -18,13 +42,19 @@ const NavItemLink = ({ id, role, href, className, children }) => {
 
 NavItemLink.defaultProps = {
   role: 'menuitem',
+  isBack: false,
 }
 
 NavItemLink.propTypes = {
   id: PropTypes.string.isRequired,
   role: PropTypes.string,
   href: PropTypes.string.isRequired,
+  isBack: PropTypes.bool,
   className: PropTypes.string,
+  onClick: PropTypes.func,
+  onKeyDown: PropTypes.func,
+  ariaHaspopup: PropTypes.string,
+  ariaControls: PropTypes.string,
   children: PropTypes.node.isRequired,
 }
 
