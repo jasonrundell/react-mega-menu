9523cec29e1bef6aa234452483e9f9d539aea2ab
Jason Rundell <jason.rundell@gmail.com>
2020-10-06 00:20:40 -0400

    NavItem updates

--- src/components/MegaMenu/NavItem/index.jsx
+++ src/components/MegaMenu/NavItem/index.jsx
@@ -7,11 +7,20 @@ import { classNames } from '../../../utils/css'
 // CSS
 import styles from './index.module.scss'
 
-const NavItem = ({ id, role, isHeading, hasChildren, className, children }) => {
+const NavItem = ({
+  id,
+  role,
+  isHeading,
+  isChildren,
+  isForward,
+  className,
+  children,
+}) => {
   const rootClasses = classNames(
     styles.root,
     isHeading && styles['heading'],
-    hasChildren && styles['has-children'],
+    isChildren && styles['children'],
+    isForward && styles['forward'],
     className && className
   )
   return (
@@ -25,13 +34,15 @@ NavItem.defaultProps = {
   role: 'none',
   isHeading: false,
   hasChildren: false,
+  isForward: false,
 }
 
 NavItem.propTypes = {
   id: PropTypes.string.isRequired,
   role: PropTypes.string,
   isHeading: PropTypes.bool,
-  hasChildren: PropTypes.bool,
+  isChildren: PropTypes.bool,
+  isForward: PropTypes.bool,
   className: PropTypes.string,
   children: PropTypes.node.isRequired,
 }
