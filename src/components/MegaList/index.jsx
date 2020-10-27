6e4de46753c0a026c311044066d62e3d508eff0a
Jason Rundell <jason.rundell@gmail.com>
2020-10-26 23:17:28 -0400

    Big cleanup of dead styles in MegaList

--- src/components/MegaList/index.jsx
+++ src/components/MegaList/index.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+// Utils
+import { classNames } from '../../utils/css'
+
 // CSS
 import styles from './index.module.scss'
 
 const MegaList = ({ children, activeState, id }) => {
+  const rootClasses = classNames(
+    styles['root'],
+    activeState && styles[activeState]
+  )
   return (
-    <ul
-      role="menu"
-      className={`${styles['root']} ${styles['sub']} ${styles['mega']} ${styles['dropdown']} ${styles[activeState]}`}
-      id={id}
-      aria-labelledby={id}
-    >
+    <ul role="menu" className={rootClasses} id={id} aria-labelledby={id}>
       {children}
     </ul>
   )
