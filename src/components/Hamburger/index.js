9f1796df5a9dd6abbb3ac728c37409b6defe93ef
Jason Rundell <jason.rundell@gmail.com>
2023-10-20 12:12:58 -0400

    Build update

--- src/components/Hamburger/index.js
+++ src/components/Hamburger/index.js
@@ -1,14 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+
+import { breakpoints as BP } from '../../config/styles'
 
 const Hamburger = ({ label, state, onClick }) => {
+  const respondTo = (breakpoint) => {
+    const breakpoints = {
+      large: `@media (min-width: ${BP.large['min-width']})` // Example breakpoint for 'large'. You can adjust this value.
+      // Add other breakpoints as needed
+    }
+    return breakpoints[breakpoint] || null
+  }
+
+  const StyledHamburger = styled.button`
+    border: none;
+    background: none;
+    transition: 0.5s ease-in-out;
+    position: absolute;
+    top: 5.25rem;
+    left: 1rem;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    z-index: 1000;
+
+    ${respondTo('large')} {
+      display: none;
+    }
+  `
+
   if (state === '') {
     state = 'rmm__hamburger--closed'
   } else if (state === 'open') {
     state = 'rmm__hamburger--open'
   }
   return (
-    <button className={`rmm__hamburger ${state}`} onClick={onClick}>
+    <StyledHamburger className={`rmm__hamburger ${state}`} onClick={onClick}>
       <div className="rmm_hamburger--slice-container">
         <span className="rmm_hamburger--slice"></span>
         <span className="rmm_hamburger--slice"></span>
@@ -20,7 +48,7 @@ const Hamburger = ({ label, state, onClick }) => {
           <span className="rmm_hamburger--label">{label}</span>
         </div>
       )}
-    </button>
+    </StyledHamburger>
   )
 }
 
---
+++ src/config/styles.js
@@ -0,0 +1,14 @@
+export const breakpoints = {
+  small: {
+    'min-width': '23rem'
+  },
+  medium: {
+    'min-width': '48rem'
+  },
+  large: {
+    'min-width': '64rem'
+  },
+  xlarge: {
+    'min-width': '75rem'
+  }
+}
