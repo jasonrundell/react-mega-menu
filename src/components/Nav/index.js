2bf764fb72a8c524a7c7bd87c6ec19dc7554b941
Jason Rundell <jason.rundell@gmail.com>
2023-10-20 13:30:58 -0400

    Update

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,19 +1,93 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+import { breakpoints as BreakPoints } from '../../config/styles'
 
-// Utils
-import { classNames } from '../../utils/css'
+const respondTo = (breakpoint) => {
+  const breakpoints = {
+    large: `@media (min-width: ${BreakPoints.large['min-width']})`
+  }
+  return breakpoints[breakpoint] || null
+}
 
-const Nav = ({ id, ariaLabel, activeState, children }) => {
-  const rootClasses = classNames(
-    'rmm__nav',
-    activeState && `rmm__nav--${activeState}`
-  )
+const StyledNav = styled.nav`
+  position: absolute;
+  top: 8rem;
+  left: -100%;
+  width: 100%;
+  height: calc(100vh - 4rem);
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  border-top: 0.0625rem solid grey;
+  padding: 1rem;
+  border-right: 0.25rem solid #000;
+  overflow-y: scroll;
 
+  ${respondTo('large')} {
+    top: 4rem;
+    left: 0;
+    border-top: none;
+    height: 4rem;
+    flex-direction: row;
+    border-right: none;
+    border-bottom: 0.0625rem solid #000;
+    overflow-y: initial;
+  }
+
+  ${({ activeState }) =>
+    activeState === 'open' &&
+    `
+    @media (prefers-reduced-motion: reduce) {
+      transform: translate3d(100%, 0, 0);
+    }
+
+    ${respondTo('large')} {
+      animation: none;
+      display: flex;
+      opacity: 1;
+
+      @media (prefers-reduced-motion: reduce) {
+        transform: none;
+      }
+    }
+  `}
+
+  ${({ activeState }) =>
+    activeState === 'closed' &&
+    `
+    @media (prefers-reduced-motion: reduce) {
+      transform: translate3d(-100%, 0, 0);
+    }
+
+    ${respondTo('large')} {
+      animation: none;
+      display: none;
+      opacity: 0;
+
+      @media (prefers-reduced-motion: reduce) {
+        transform: none;
+      }
+    }
+  `}
+
+  li:first-child {
+    ${respondTo('large')} {
+      margin-left: 0;
+    }
+  }
+`
+
+const Nav = ({ id, ariaLabel, activeState, children, ...props }) => {
   return (
-    <nav id={id} className={rootClasses} aria-label={ariaLabel}>
+    <StyledNav
+      id={id}
+      activeState={activeState}
+      aria-label={ariaLabel}
+      {...props}
+    >
       {children}
-    </nav>
+    </StyledNav>
   )
 }
 
