79ed6f5ee04920e933319074c93cf0355d8beb8a
Jason Rundell <jason.rundell@gmail.com>
2023-10-20 14:36:28 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import styled from '@emotion/styled'
-import { breakpoints as BreakPoints } from '../../config/styles'
+import { breakpoints as BreakPoints } from './config/styles'
 
 // Components
 import TopBar from './components/TopBar'
